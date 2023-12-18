1f383d8faa0049d300137cc16ef2b54c291afd60
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-12-18 17:29:52 +0530

    Initial Commit

---
+++ Apple_frameworks/Apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_frameworksApp.swift
+//  Apple_frameworks
+//
+//  Created by Diptayan Jash on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
